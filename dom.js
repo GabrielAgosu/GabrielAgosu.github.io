0914749687549da8f196e2b48371c41ee1d6ca4b
GabrielAgosu <gabrielagosu08@gmail.com>
2024-08-06 18:23:03 +0000

    added js aspect and bootstrap wirefram for final project

--- dom.js
+++ dom.js
@@ -1,7 +1,7 @@
 let colorbutton =document.querySelector("#please")
 
 function changebackgroundcolor(){
-    document.body.style.backgroundColor ='black'
+    document.body.style.backgroundColor ='black';document.body.style.color="white"
 }
 
 colorbutton.addEventListener("click", changebackgroundcolor);
@@ -11,7 +11,7 @@ colorbutton.addEventListener("click", changebackgroundcolor);
 let colorbutton2 =document.querySelector("#gabe")
 
 function changebackgroundcolor2(){
-    document.body.style.backgroundColor ='yellow'
+    document.body.style.backgroundColor ='yellow';document.body.style.color="black"
 }
 
 colorbutton2.addEventListener("click", changebackgroundcolor2);
