b11f07ddd4a120305c6ceda02043c81e4392539a
GabrielAgosu <gabrielagosu08@gmail.com>
2024-08-06 17:58:36 +0000

    final project

---
+++ dom.js
@@ -0,0 +1,21 @@
+let colorbutton =document.querySelector("#please")
+
+function changebackgroundcolor(){
+    document.body.style.backgroundColor ='black'
+}
+
+colorbutton.addEventListener("click", changebackgroundcolor);
+
+
+
+let colorbutton2 =document.querySelector("#gabe")
+
+function changebackgroundcolor2(){
+    document.body.style.backgroundColor ='yellow'
+}
+
+colorbutton2.addEventListener("click", changebackgroundcolor2);
+
+
+
+
