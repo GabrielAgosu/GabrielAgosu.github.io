99eb200e957582d16d9582b4d6e7ae0d94c032b9
GabrielAgosu <gabrielagosu08@gmail.com>
2024-08-12 05:45:43 +0000

    almost complete with final priject added base requirements

--- dom.js
+++ dom.js
@@ -1,11 +1,23 @@
+document.body.style.fontFamily='justanotherhand'
 document.body.style.backgroundColor= 'silver'
 document.body.style.color='black'
+
+
+let resetbutton=document.querySelector('#reset')
+
+function resetpage(){
+    document.body.style.color=('black');document.body.style.backgroundColor=('silver')
+}
+resetbutton.addEventListener('click', resetpage)
+
+
+
 
 
 let colorbutton =document.querySelector("#please")
 
 function changebackgroundcolor(){
-    document.body.style.backgroundColor ='black';document.body.style.color="white"
+    document.body.style.backgroundColor ='black';document.body.style.color='white'
 }
 
 colorbutton.addEventListener("click", changebackgroundcolor);
@@ -15,10 +27,16 @@ colorbutton.addEventListener("click", changebackgroundcolor);
 let colorbutton2 =document.querySelector("#gabe")
 
 function changebackgroundcolor2(){
-    document.body.style.backgroundColor ='yellow';document.body.style.color="black"
+    document.body.style.backgroundColor ='white';document.body.style.color="black"
 }
 
 colorbutton2.addEventListener("click", changebackgroundcolor2);
+
+
+
+
+
+
 
 
 
---
+++ mom.js
@@ -0,0 +1,3 @@
+document.body.style.fontFamily='justanotherhand'
+document.body.style.backgroundColor= 'silver'
+document.body.style.color='black'
